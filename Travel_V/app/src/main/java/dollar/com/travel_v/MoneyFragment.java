f6b366864b094ff58a2387b236feb62cdd1f8601
anhchuoi0k123 <anhchuoi0k@gmail.com>
2017-03-21 10:21:34 +0700

    moi ne`
    lala

--- Travel_V/app/src/main/java/dollar/com/travel_v/MoneyFragment.java
+++ Travel_V/app/src/main/java/dollar/com/travel_v/MoneyFragment.java
@@ -1,6 +1,7 @@
 package dollar.com.travel_v;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +20,11 @@ public class MoneyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.money_layout, container, false);
 
-
-
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnTroVe = (Button) view.findViewById(R.id.tro_ve);
+    }
 }
